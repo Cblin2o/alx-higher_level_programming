242be51e55b2bfda8841abbc4f5e54b81987a196
Cblin2o <kolawolerasheed59@gmail.com>
2023-12-04 14:42:16 -0800

    r

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+    else:
+        a = my_list
+        for i in range(len(a)):
+            idx = element
+            print("{:d}".format(a[i]))
