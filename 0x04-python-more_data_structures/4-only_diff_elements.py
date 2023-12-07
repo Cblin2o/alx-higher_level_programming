2f0dda722d6cd7b7ecabc7e813677f662e197990
Cblin2o <kolawolerasheed59@gmail.com>
2023-12-06 16:40:21 -0800

    e

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    difference = set()
-    common = set()
-    for i in (set_1,set_2):
-        difference = (difference | i) - (i & common)
-        common | = i
-    return difference
+    return (set_1 ^ set_2)
