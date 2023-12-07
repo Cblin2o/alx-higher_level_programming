cf02a49b3b844b6390dae63878388d37ce26a9c0
Cblin2o <kolawolerasheed59@gmail.com>
2023-12-06 16:05:23 -0800

    r

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,4 @@
 def search_replace(my_list, search, replace):
     if not my_list:
         return my_list
-    my_list[:] = [x if x != search else replace for x in my_list]
+    return [x if x != search else replace for x in my_list]
