fac375e3c708b2224dd1f21881d8f0ab83af81e6
Cblin2o <kolawolerasheed59@gmail.com>
2023-12-18 18:00:22 -0800

    e

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,6 +4,9 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]))
+        except IndexError:
+            break
+        else:
             loop += 1
     print()
     
