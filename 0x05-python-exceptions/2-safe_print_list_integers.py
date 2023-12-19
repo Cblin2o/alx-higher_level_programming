d808d7407660345afcf29119d7b620cef944ff59
Cblin2o <kolawolerasheed59@gmail.com>
2023-12-18 18:30:37 -0800

    w

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    loop = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]))
+        except (TypeError, ValueError):
+            pass
+        else:
+            loop += 1
+    print()
+    return loop
